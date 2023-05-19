41051a9620a0a5916eacb2b996a7fc3a0070839a
Tal Mekler <tal.mekler11@gmail.com>
2023-05-19 23:47:52 +0300

    PAT-84 Ctor->get host and port

--- src/Model/ClientCommunication.java
+++ src/Model/ClientCommunication.java
@@ -1,7 +1,17 @@
 package Model;
 
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Observable;
+
 public class ClientCommunication extends Observable {
     Socket socket;
+
+    public ClientCommunication(String host, int port) {
+        try {
+            socket = new Socket(host, port);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
