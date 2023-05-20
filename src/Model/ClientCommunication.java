6e52b7ef1006205327c02dbe63de39660e65ad57
Tal Mekler <tal.mekler11@gmail.com>
2023-05-20 16:35:47 +0300

    PAT-84 check if the server send to client msg and notify oversers

--- src/Model/ClientCommunication.java
+++ src/Model/ClientCommunication.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Observable;
+import java.util.Scanner;
 
 public class ClientCommunication extends Observable {
     Socket socket;
 
     /**
      * Creates a new client communication
+     *
      * @param host the host to connect to
      * @param port the port to connect to
      */
@@ -23,18 +25,37 @@ public class ClientCommunication extends Observable {
 
     /**
      * Sends a message to the server
-     * @param id the id of the client
+     *
+     * @param id         the id of the client
      * @param methodName the method name to call
-     * @param inputs the inputs to the method
+     * @param inputs     the inputs to the method
      */
     public void send(int id, String methodName, String... inputs) {
-        String message = id+":"+methodName+":"+String.join(",", inputs);
+        String message = id + ":" + methodName + ":" + String.join(",", inputs);
         try {
-            PrintWriter out=new PrintWriter(socket.getOutputStream());
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
             out.println(message);
             out.flush();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * check if the server send a message and notify the observers
+     */
+    public void checkForMessage() {
+        while (socket.isConnected() && !socket.isClosed()) {
+            try {
+                Scanner in = new Scanner(socket.getInputStream());
+                if (in.hasNext()) {
+                    String message = in.nextLine();
+                    setChanged();
+                    notifyObservers(message);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
