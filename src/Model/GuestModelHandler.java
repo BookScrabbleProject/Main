65921812b3421ba1654ee9af20c564d600486105
YuvalOfek1 <yuvalofek97@gmail.com>
2023-05-20 22:14:20 +0300

    PAT-85 added hostServer constructor + added guestModelHandler class with methods

---
+++ src/Model/GuestModelHandler.java
@@ -0,0 +1,17 @@
+package Model.tests;
+
+import Model.gameClasses.ClientHandler;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class GuestModelHandler implements ClientHandler {
+    public void handleClient(InputStream inFromclient, OutputStream outToClient) {
+        // TODO implement here
+    }
+
+    public void close() {
+        // TODO implement here
+    }
+
+}
