3e66a3c26344118b5977aec66f7d0867c3d324c2
OmerBendaa <omerbd29@gmail.com>
2023-05-28 16:34:16 +0300

    PAT-88

--- src/Model/tests/HostModelTest.java
+++ src/Model/tests/HostModelTest.java
@@ -73,60 +73,87 @@ public class HostModelTest {
         else System.out.println("There is ERROR in one or more tests");
     } catch (IOException | InterruptedException e) {throw new RuntimeException(e);}
 }
-    // todo - need to upload words before testing ->BoardUpdatedTest
 
-    /**
+    public static void takeTileFromBagTest(Socket socket,Socket socket2,HostModel hostModel,int hostServerPort){
+        hostModel.update(null,"1:takeTileFromBag:1");
+        try {
+            Scanner scanner1=new Scanner(socket.getInputStream());
+            String[] line1= scanner1.next().split(":");
+            if(!line1[1].equals("setHand"))
+                System.out.println("wrong message for specific player");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+  /*  // todo - need to upload words before testing ->BoardUpdatedTest
+
+
+    *//**
      * Method that test if the board was updated
      * @param socket - represent client
      * @param socket2 - represent client
-     */
+     *//*
     public static  void BoardUpdatedTest(Socket socket, Socket socket2){
-        try {
+        *//*try {
         PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
-        } catch (IOException e) {throw new RuntimeException(e);}
+        } catch (IOException e) {throw new RuntimeException(e);}*//*
     }
 
-    /**
+    *//**
      * Method that test the hand of the players and number of tiles in other players
      * @param socket - represent client
      * @param socket2 - represent client
-     */
+     *//*
     public static void HandUpdateTest(Socket socket, Socket socket2){}
 
-    /**
+    *//**
      * Method that test of the turn was passed
      * @param socket - represent client
      * @param socket2 - represent client
-     */
+     *//*
     public static  void passTheTurnTest(Socket socket, Socket socket2){}
 
-    /**
+    *//**
      * Mehod that test if the update in case tryPlaceWord update all the players and worked
      * @param hostModel - represent the host model
-     */
+     *//*
     public static void tryPlaceWordUpdateTests(HostModel hostModel) {
-        hostModel.update(null,"1:tryPlaceWord:1");
+        *//*hostModel.update(null,"1:tryPlaceWord:1");
         hostModel.update(null,"2:tryPlaceWord:0");
-        hostModel.update(null,"3:tryPlaceWord:1");
+        hostModel.update(null,"3:tryPlaceWord:1");*//*
     }
-    /**
+    *//**
      * Method that test if the update in case challenge update all the players and worked
      * @param hostModel - represent the host model
-     */
+     *//*
     public static void challengeUpdateTests(HostModel hostModel) {
-        hostModel.update(null,"1:challenge:0");
+        *//*hostModel.update(null,"1:challenge:0");
         hostModel.update(null,"2:challenge:0");
-        hostModel.update(null,"3:challenge:1");
+        hostModel.update(null,"3:challenge:1");*//*
     }
-    /**
+    *//**
      * Method that test if the update in case takeTileFromBag update all the players and worked
-     * @param hostModel - represent the host model
-     */
+     //* @param hostModel - represent the host model
+     *//*
     public static void takeTileFromBagUpdateTests(HostModel hostModel) {
-        hostModel.update(null,"1:takeTileFromBag");
+        hostModel.
+
+
+                hostModel.update(null,"1:takeTileFromBag");
         hostModel.update(null,"2:takeTileFromBag");
         hostModel.update(null,"3:takeTileFromBag");
-    }
+    }*/
     public static void main(String[] args) {
 
         HostModel hostModel = HostModel.getHost();
@@ -137,13 +164,17 @@ public class HostModelTest {
             Socket socket = new Socket("localhost",hostServerPort);
             Socket socket2 = new Socket("localhost",hostServerPort);
             addPlayerTest(socket,socket2, hostModel,hostServerPort); // test adding players to the server
-            BoardUpdatedTest(socket,socket2); // test if the board updated
+
+
+            /* BoardUpdatedTest(socket,socket2); // test if the board updated
             HandUpdateTest(socket,socket2); // refill hand + take tile from bag
             passTheTurnTest(socket,socket2); // pass the turn test
             tryPlaceWordUpdateTests(hostModel); // test for case try place word in update
             challengeUpdateTests(hostModel); // test for case challenge in update
-            takeTileFromBagUpdateTests(hostModel); // test for case take tile from bag in update
 
+
+            takeTileFromBagUpdateTests(hostModel); // test for case take tile from bag in update
+*/
         } catch (IOException e) {throw new RuntimeException(e);}
     }
 
