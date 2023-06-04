f7c098ce0ca1f4a1c2e53467810ae3f5cc0d99bd
Tal Mekler <tal.mekler11@gmail.com>
2023-06-04 13:34:48 +0300

    PAT-90 start methods' docs and implementation

--- src/ViewModel/ViewModel.java
+++ src/ViewModel/ViewModel.java
@@ -2,6 +2,7 @@ package ViewModel;
 
 import Model.Model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ViewModel {
@@ -13,34 +14,60 @@ public class ViewModel {
     List<Character> myHand;
 
     public ViewModel() {
-        // TODO: Implement
+        board = new Character[15][15];
+        changesList = new ArrayList<>();
+        currentPlayerId = 0;
+        myHand = new ArrayList<>();
+        // Todo: add more initialization here
     }
 
+    /**
+     * Run when the player clicks the "Place Word" button.
+     * Tries to place the word on the board if it is valid (from changesList) using the Model.
+     * If the word is invalid, notify the view
+     */
     public void tryPlaceWord() {
-        // TODO: Implement
+        if(isChangeValid()) {
+            model.tryPlaceWord(getWord(), getWordStartRow(), getWordStartCol(), isWordVertical());
+            return;
+        }
+        // Todo: notify the view that the change is invalid
     }
 
+    /**
+     * @return true if the word is valid, false otherwise (from changesList)
+     */
     private boolean isChangeValid() {
         // TODO: Implement
 
         return false;
     }
 
+    /**
+     * @return the word that the player is trying to place
+     */
     private String getWord() {
         // TODO: Implement
         return null;
     }
 
+    /**
+     * @return the row of the first letter of the word
+     */
     private int getWordStartRow() {
-        // TODO: Implement
-        return -1;
+        return changesList.get(0).getNewRow();
     }
 
+    /**
+     * @return the column of the first letter of the word
+     */
     private int getWordStartCol() {
-        // TODO: Implement
-        return -1;
+        return changesList.get(0).getNewCol();
     }
 
+    /**
+     * @return true if the word is vertical, false if it is horizontal
+     */
     private boolean isWordVertical() {
         // TODO: Implement
         return false;
