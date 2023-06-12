8f9d1dfe017500495829f16dfe62a32fd8620efa
Tal Mekler <tal.mekler11@gmail.com>
2023-06-12 16:58:10 +0300

    PAT-90 .

--- src/ViewModel/tests/ViewModelTest.java
+++ src/ViewModel/tests/ViewModelTest.java
@@ -1,4 +1,5 @@
 package ViewModel.tests;
+
 import Model.HostModel;
 import Model.GuestModel;
 import Model.gameClasses.BookScrabbleHandler;
@@ -8,33 +9,38 @@ import ViewModel.ViewModel;
 public class ViewModelTest {
 
     public static void main(String[] args) {
-        MyServer server = new MyServer(1234, new BookScrabbleHandler());
-        HostModel host = HostModel.getHost();
-        host.connectToBookScrabbleServer(1235, "localhost", 1234);
-        GuestModel guest = new GuestModel("localhost", 1235, "guest");
-
-        ViewModel viewModel = new ViewModel(guest);
-        guest.connectToHostServer();
-
         try {
+
+
+            MyServer server = new MyServer(1234, new BookScrabbleHandler());
+            server.start();
+            HostModel host = HostModel.getHost();
+            host.connectToBookScrabbleServer(1235, "localhost", 1234);
+            GuestModel guest = new GuestModel("localhost", 1235, "guest");
+
+            ViewModel viewModel = new ViewModel(guest);
+            guest.connectToHostServer();
+
+            guest.tryPlaceWord("hello", 7, 7, true);
+
             Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
 
-        GuestModel guest2 = new GuestModel("localhost", 1235, "guest2");
-        guest2.connectToHostServer();
-        try {
+            GuestModel guest2 = new GuestModel("localhost", 1235, "guest2");
+            guest2.connectToHostServer();
+
             Thread.sleep(2000);
+
+            guest.tryPlaceWord("hello", 7, 7, true);
+            Thread.sleep(2000);
+
+            server.stop();
+            host.getHostServer().close();
+            guest.getClientCommunication().close();
+            guest2.getClientCommunication().close();
+
+            System.out.println("done!");
         } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
-
-        server.stop();
-        host.getHostServer().close();
-        guest.getClientCommunication().close();
-        guest2.getClientCommunication().close();
-
-        System.out.println("done!");
     }
 }
