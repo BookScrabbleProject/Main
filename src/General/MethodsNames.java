61464f2e40a73150f88730e0706659f5f4a99520
Tal Mekler <tal.mekler11@gmail.com>
2023-06-17 07:45:45 +0300

    add static strings -> MethodsNames

--- src/General/MethodsNames.java
+++ src/General/MethodsNames.java
@@ -1,16 +1,15 @@
 package General;
 
-public enum MethodsNames {
-    BOARD_UPDATED,
-    SCORE_UPDATED,
-    NUM_OF_TILES_UPDATED,
-    SET_HAND,
-    NEW_PLAYER_TURN,
-    SET_ID,
-    PLAYER_LIST_UPDATED,
-    START_GAME,
-    CHALLENGE,
-    TRY_PLAYERS_WORD,
-    TAKE_TILE_FROM_BAG,
-    CONNECT
+public class MethodsNames {
+    public static String TRY_PLACE_WORD = "tryPlaceWord";
+    public static String CHALLENGE = "challenge";
+    public static String START_GAME = "startGame";
+    public static String BOARD_UPDATED = "boardUpdated";
+    public static String SCORE_UPDATED = "scoreUpdated";
+    public static String NUM_OF_TILES_UPDATED = "numOfTilesUpdated";
+    public static String SET_HAND = "setHand";
+    public static String NEW_PLAYER_TURN = "newPlayerTurn";
+    public static String SET_ID = "setId";
+    public static String PLAYERS_LIST_UPDATED = "playersListUpdated";
+    public static String CONNECT = "connect";
 }
