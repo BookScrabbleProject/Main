7e83fea58a3b3f0875f0254a2063298358315c38
YuvalOfek1 <yuvalofek97@gmail.com>
2023-06-23 16:05:49 +0300

    PAT-92 can insert tiles to board

--- src/main/java/View/bookscrabbleapp/InGameController.java
+++ src/main/java/View/bookscrabbleapp/InGameController.java
@@ -20,6 +20,7 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -92,45 +93,55 @@ public class InGameController implements Observer, Initializable {
     private Circle player3ImageCircle;
     @FXML
     private Circle myPlayerImageCircle;
-    Alert challengeAlert;
 
+    boolean isGameStarted = false;
+    private Alert challengeAlert;
     private Image userIcon = new Image(getClass().getResource("/Images/userIcon.jpg").toExternalForm());
 
+    //in every index we can find the player's id. the first player that we see in the game is the player in index 0 with id X
+    int[] playersPosition = new int[4];
     Label[] playersNames = new Label[4];
     Label[] playersScores = new Label[4];
     ImageView[] playersImages = new ImageView[4];
     Circle[] playersImagesCircles = new Circle[4];
-
-
-    boolean isGameStarted = false;
-    Character pickedTile = null;
-
-    //in every index we can find the player's id. the first player that we see in the game is the player in index 0 with id X
-    int[] playersPosition = new int[4];
-
-
-
     final int BOARD_HGAP = 11;
     final int BOARD_VGAP = 4;
     final int HAND_HGAP = 20;
+
+    double SQUARE_WIDTH; //the width of each square in the board
+    double SQUARE_HEIGHT; //the height of each square in the board
+
+
+    Character lastPickedTile = null;
+    int lastPickedTileIndex = -1;
+    Character[][] boardStatus = new Character[15][15];
+
+
+    ArrayList<DataChanges> dataChangesList = new ArrayList<>();
+    ArrayList<Integer> indexChangesList = new ArrayList<>();
+
+
     public void boardClickHandler(MouseEvent e){
-        DataChanges data1 = new DataChanges('w', 7,7,-1,-1);
-        DataChanges data2 = new DataChanges('e', 7,8,-1,-1);
-        DataChanges data3 = new DataChanges('e', 7,9,-1,-1);
-        DataChanges data4 = new DataChanges('k', 7,10,-1,-1);
-        ViewModel.getViewModel().changesList.add(data1);
-        ViewModel.getViewModel().changesList.add(data2);
-        ViewModel.getViewModel().changesList.add(data3);
-        ViewModel.getViewModel().changesList.add(data4);
-        ViewModel.getViewModel().tryPlaceWord();
+//        DataChanges data1 = new DataChanges('w', 7,7);
+//        DataChanges data2 = new DataChanges('e', 7,8);
+//        DataChanges data3 = new DataChanges('e', 7,9);
+//        DataChanges data4 = new DataChanges('k', 7,10);
+//        ViewModel.getViewModel().changesList.add(data1);
+//        ViewModel.getViewModel().changesList.add(data2);
+//        ViewModel.getViewModel().changesList.add(data3);
+//        ViewModel.getViewModel().changesList.add(data4);
+//        ViewModel.getViewModel().tryPlaceWord();
         if(ViewModel.getViewModel().getMyPlayer().getId()!=ViewModel.getViewModel().getCurrentPlayerId())
+            return;
+        if(lastPickedTile== null || lastPickedTileIndex == -1)
             return;
         double MOUSE_CLICKED_X = e.getX(); //the y of the mouse click relative to the scene
         double MOUSE_CLICKED_Y = e.getY(); //the x of the mouse click relative to the scene
 
         System.out.println("x: " + MOUSE_CLICKED_X + " y: " + MOUSE_CLICKED_Y);
-        double SQUARE_WIDTH = (gridPane.getWidth()-BOARD_HGAP*14) / 15; //the width of each square
-        double SQUARE_HEIGHT = (gridPane.getHeight()-BOARD_VGAP*14) / 15; //the height of each square
+        SQUARE_WIDTH = (gridPane.getWidth()-BOARD_HGAP*14) / 15;
+        SQUARE_HEIGHT= (gridPane.getHeight()-BOARD_VGAP*14) / 15; //the height of each square in the board
+
         int cr = (int) Math.floor (MOUSE_CLICKED_Y / (SQUARE_HEIGHT+BOARD_VGAP)); //the row of the square clicked
         int cc = (int) Math.floor (MOUSE_CLICKED_X / (SQUARE_WIDTH+BOARD_HGAP)); //the column of the square clicked
         boolean clickedInGap = (MOUSE_CLICKED_X - (cc * (SQUARE_WIDTH + BOARD_HGAP))) > SQUARE_WIDTH;
@@ -142,6 +153,7 @@ public class InGameController implements Observer, Initializable {
         System.out.println("clicked in gap: " + clickedInGap);
         System.out.println("square width: " + SQUARE_WIDTH + " square height: " + SQUARE_HEIGHT + " grid width: " + gridPane.getWidth() + " grid height: " + gridPane.getHeight() + " "+ (MOUSE_CLICKED_X-cc*(SQUARE_WIDTH+8)));
         GraphicsContext gc = gameBoard.getGraphicsContext2D();
+        addTileToBoard(lastPickedTile, lastPickedTileIndex, cr, cc);
         if(!boardImage.isVisible())
             boardImage.setVisible(true);
 
@@ -163,68 +175,7 @@ public class InGameController implements Observer, Initializable {
                 }
             }
         }
-        ViewModel.getViewModel().takeTileFromBag();
 
-    }
-
-
-
-    @Override
-    public void update(Observable o, Object arg) {
-        System.out.println("View update "+arg);
-        String[] splitedArgString = ((String)arg).split(":");
-        String methodName = splitedArgString[0];
-        String arguments=splitedArgString.length>1?splitedArgString[1]:"";
-        switch (methodName){
-            case MethodsNames.DISCONNECT_FROM_SERVER:
-                System.out.println("In Case Disconnect");
-                Platform.runLater(this::showBackAlert);
-                System.out.println("Alert Created");
-                break;
-            case MethodsNames.SET_HAND:
-                Platform.runLater(this::setHand);
-                break;
-            case MethodsNames.NEW_PLAYER_TURN:
-                Platform.runLater(this::newPlayerTurn);
-                break;
-            case MethodsNames.TRY_PLACE_WORD:
-                Platform.runLater(()->{
-                    System.out.println("In Try Place Word Case");
-                    String[] splitedArguments=arguments.split(",");
-                    if(splitedArguments[0].equals("0"))
-                    {
-                        //todo : need to make popUp for the player that his word is invalid
-                    }
-                    else{
-                        if(ViewModel.getViewModel().getCurrentPlayerId()!=ViewModel.getViewModel().getMyPlayer().getId()){
-                            List<String>words=new ArrayList<>(splitedArguments.length-1);
-                            for(int i=1;i<splitedArguments.length;i++){
-                                words.add(splitedArguments[i]);
-                            }
-                            String[] wordsArray=new String[words.size()];
-                            wordsArray=words.toArray(wordsArray);
-                            showChallengeAlert(wordsArray);
-                        }
-                    }
-                });
-                break;
-            case MethodsNames.CHALLENGE:
-                //todo challenge success/fail logic
-                break;
-            case MethodsNames.CLOSE_CHALLENGE_ALERT:
-                Platform.runLater(()->{
-                    if(challengeAlert.isShowing())
-                        challengeAlert.close();
-                });
-                break;
-            case MethodsNames.INVALID_PLACEMENT:
-                //todo pop a popUp for the player that tried to put the word, that his placement is invalid
-                break;
-            case MethodsNames.END_GAME:
-                //todo pop a popUp that the game has ended->victory popUp
-                //we get the players id-score list ordered by the descending scores
-                break;
-        }
 
     }
 
@@ -267,11 +218,64 @@ public class InGameController implements Observer, Initializable {
             iv.setFitHeight(HAND_SQUARE_HEIGHT);
             StackPane sp = new StackPane(iv);
             sp.setAlignment(Pos.CENTER);
+            int finalI = i;
+            sp.setOnMouseClicked((event)-> {
+                if(sp.isVisible())
+                    tileInHandClickHandler(event, finalI, myC);
+            });
             tilesInHandGrid.add(sp, i, 0);
 
             System.out.println("Added stackpane to hand gridpane");
         }
     }
+
+    private void tileInHandClickHandler(MouseEvent event, int index, Character myC) {
+        if(ViewModel.getViewModel().getMyPlayer().getId()!=ViewModel.getViewModel().getCurrentPlayerId())
+            return;
+        if(event.getButton() == MouseButton.PRIMARY) {
+            for(int i=0; i<7; i++){
+                if(i==index && tilesInHandGrid.getChildren().get(i).getStyle().equals("")){ //if the tile is not selected
+                    tilesInHandGrid.getChildren().get(i).setStyle("-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.8), 20, 0, 0, 0);");
+                }
+                else{
+                    tilesInHandGrid.getChildren().get(i).setStyle("");
+                }
+            }
+            if(lastPickedTileIndex == index){
+                lastPickedTileIndex = -1;
+                lastPickedTile = null;
+            }
+            else{
+                lastPickedTileIndex = index;
+                lastPickedTile = myC;
+            }
+
+        }
+    }
+
+    private void addTileToBoard(Character letter, int indexInHand, int row, int col ){
+        if(letter==null || indexInHand<0 || indexInHand >=7 || row>14 || row<0 || col>14 || col<0) return;
+        if(boardStatus[row][col]!=null && boardStatus[row][col].charValue()>='A' && boardStatus[row][col].charValue()<='Z') return;
+        if(boardStatus[row][col]!=null && boardStatus[row][col].charValue()>='a' && boardStatus[row][col].charValue()<='z') return;
+        tilesInHandGrid.getChildren().get(indexInHand).setVisible(false);
+        tilesInHandGrid.getChildren().get(indexInHand).setStyle("");
+        boardStatus[row][col] = letter;
+//        Image tile = new Image(getClass().getResource("/Images/Tiles/" + letter + "Letter.png").toExternalForm());
+//        ImageView iv = new ImageView(tile);
+//        iv.setFitWidth(SQUARE_WIDTH-3);
+//        iv.setFitHeight(SQUARE_HEIGHT-2);
+//        StackPane sp = new StackPane(iv);
+//        sp.setAlignment(Pos.CENTER);
+//        gridPane.add(sp, col, row);
+        drawBoard();
+        dataChangesList.add(new DataChanges(letter, row, col));
+        indexChangesList.add(indexInHand);
+        lastPickedTile = null;
+        lastPickedTileIndex = -1;
+
+
+    }
+
     public void showChallengeAlert(String... words){
         challengeAlert = new Alert(Alert.AlertType.INFORMATION);
         challengeAlert.setTitle("Challenge");
@@ -309,9 +313,6 @@ public class InGameController implements Observer, Initializable {
         alert.getButtonTypes().setAll(buttonTypeOne);
 
 
-
-
-
         alert.setOnCloseRequest(event -> {
             System.out.println("Alert Closed");
             ((Stage) alert.getDialogPane().getScene().getWindow()).close();
@@ -343,6 +344,7 @@ public class InGameController implements Observer, Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ViewModel.getViewModel().addObserver(this);
+        challengeAlert = new Alert(Alert.AlertType.INFORMATION);
         for(int i=0;i<3;i++){
             playersPosition[i] = -1;
         }
@@ -353,19 +355,22 @@ public class InGameController implements Observer, Initializable {
 
         numOfTilesInHand.textProperty().bind(ViewModel.getViewModel().getMyPlayer().numberOfTilesProperty);
         numOfTilesInBag.textProperty().bind(ViewModel.getViewModel().numberOfTilesInBagProperty);
-        myPlayerScore.textProperty().bind(ViewModel.getViewModel().getMyPlayer().scoreProperty.concat(" pts"));
+        myPlayerScore.textProperty().bind(ViewModel.getViewModel().getMyPlayer().scoreProperty);
         newPlayerTurn();
+        for(int i=0;i<15;i++){
+            for(int j=0;j<15;j++){
+                boardStatus[i][j] = '_';
+            }
+        }
 
     }
 
     private void fillPlayersDetails() {
         int myID = ViewModel.getViewModel().getMyPlayer().getId();
-        int myScore = ViewModel.getViewModel().getMyPlayer().getScore();
-        for(int i=0;i<3;i++){
+        for(int i=0;i<=3;i++){
             if(ViewModel.getViewModel().getPlayers().get(i) == null) continue;
             if(ViewModel.getViewModel().getPlayers().get(i).getId() == myID) {
                 playersPosition[i]=4;
-                myPlayerScore.textProperty().bind(ViewModel.getViewModel().getPlayers().get(i).scoreProperty.concat(" pts"));
                 playersNames[i] = myPlayerName;
                 playersImagesCircles[i] = myPlayerImageCircle;
                 playersImages[i] = myPlayerImage;
@@ -379,7 +384,7 @@ public class InGameController implements Observer, Initializable {
     public void insertPlayerDetails(String name, int id){
         if(player1Name.getText().equals("")){
             player1Name.setText(name);
-            player1Score.textProperty().bind(ViewModel.getViewModel().getPlayers().get(id).scoreProperty.concat(" pts"));
+            player1Score.textProperty().bind(ViewModel.getViewModel().getPlayers().get(id).scoreProperty);
             player1Name.setVisible(true);
             player1Score.setVisible(true);
             player1ImageCircle.setFill(new ImagePattern(userIcon));
@@ -418,6 +423,117 @@ public class InGameController implements Observer, Initializable {
             playersImagesCircles[id] = player3ImageCircle;
             playersImages[id] = player3Image;
         }
+    }
+
+    public void undoBtnClickHandler(){
+        if(dataChangesList.size()==0 || indexChangesList.size()==0) return;
+        DataChanges toRemove = dataChangesList.get(dataChangesList.size()-1);
+        boardStatus[toRemove.getNewRow()][toRemove.getNewCol()]='_';
+        drawBoard();
+        tilesInHandGrid.getChildren().get(indexChangesList.get(indexChangesList.size()-1)).setVisible(true);
+        dataChangesList.remove(dataChangesList.size()-1);
+        indexChangesList.remove(indexChangesList.size()-1);
+    }
+
+    public void resetBtnClickHandler(){
+        while(dataChangesList.size()!=0){
+            undoBtnClickHandler();
+        }
+    }
+
+    public void drawBoard(){
+        gridPane.getChildren().clear();
+        for(int i=0;i<15;i++){
+            for(int j=0;j<15;j++){
+                if(boardStatus[i][j]!='_'){
+                    System.out.println("boardStatus["+i+"]["+j+"] = "+boardStatus[i][j]);
+                    System.out.println("boardStatus["+i+"]["+j+"] lowerCase = "+Character.toLowerCase(boardStatus[i][j]));
+                    Image tile = new Image(getClass().getResource("/Images/Tiles/" + Character.toLowerCase(boardStatus[i][j]) + "Letter.png").toExternalForm());
+                    ImageView iv = new ImageView(tile);
+                    iv.setFitWidth(SQUARE_WIDTH-3);
+                    iv.setFitHeight(SQUARE_HEIGHT-2);
+                    StackPane sp = new StackPane(iv);
+                    sp.setAlignment(Pos.CENTER);
+                    gridPane.add(sp, j, i);
+                }
+            }
+        }
+    }
+
+    public void finishMyTurnBtnHandler(){
+        ViewModel.getViewModel().changesList.clear();
+        for(DataChanges data: dataChangesList){
+            ViewModel.getViewModel().changesList.add(data);
+        }
+        ViewModel.getViewModel().tryPlaceWord();
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        System.out.println("View update "+arg);
+        String[] splitedArgString = ((String)arg).split(":");
+        String methodName = splitedArgString[0];
+        String arguments=splitedArgString.length>1?splitedArgString[1]:"";
+        switch (methodName){
+            case MethodsNames.DISCONNECT_FROM_SERVER:
+                System.out.println("In Case Disconnect");
+                Platform.runLater(this::showBackAlert);
+                System.out.println("Alert Created");
+                break;
+            case MethodsNames.SET_HAND:
+                Platform.runLater(this::setHand);
+                break;
+            case MethodsNames.NEW_PLAYER_TURN:
+                Platform.runLater(this::newPlayerTurn);
+                break;
+            case MethodsNames.TRY_PLACE_WORD:
+                Platform.runLater(()->{
+                    System.out.println("In Try Place Word Case");
+                    String[] splitedArguments=arguments.split(",");
+                    if(splitedArguments[0].equals("0"))
+                    {
+                        //todo : need to make popUp for the player that his word is invalid
+                    }
+                    else{
+                        if(ViewModel.getViewModel().getCurrentPlayerId()!=ViewModel.getViewModel().getMyPlayer().getId()){
+                            List<String>words=new ArrayList<>(splitedArguments.length-1);
+                            for(int i=1;i<splitedArguments.length;i++){
+                                words.add(splitedArguments[i]);
+                            }
+                            String[] wordsArray=new String[words.size()];
+                            wordsArray=words.toArray(wordsArray);
+                            showChallengeAlert(wordsArray);
+                        }
+                        dataChangesList.clear();
+                        indexChangesList.clear();
+                    }
+                });
+                break;
+            case MethodsNames.CHALLENGE:
+                //todo challenge success/fail logic
+                break;
+            case MethodsNames.CLOSE_CHALLENGE_ALERT:
+                Platform.runLater(()->{
+                    if(challengeAlert.isShowing())
+                        challengeAlert.close();
+                });
+                break;
+            case MethodsNames.INVALID_PLACEMENT:
+                //todo pop a popUp for the player that tried to put the word, that his placement is invalid
+                break;
+            case MethodsNames.END_GAME:
+                //todo pop a popUp that the game has ended->victory popUp
+                //we get the players id-score list ordered by the descending scores
+                break;
+
+            case MethodsNames.BOARD_UPDATED:
+                Platform.runLater(()->{
+                    boardStatus = ViewModel.getViewModel().getBoard();
+                    drawBoard();
+                });
+                break;
+        }
+
     }
 }
 
